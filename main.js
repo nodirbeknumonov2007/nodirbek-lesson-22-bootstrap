5d4066ecbb3ef5309d2cc02fbe818648cef229e7
nodirbeknumonov2007 <nodirbeknumonov2007@gmail.com>
2023-04-13 04:07:25 +0500

    Lesson-22

---
+++ main.js
@@ -0,0 +1,9 @@
+// const loading = document.getElementById("loading");
+// window.addEventListener("load", () => {
+//   loading.classList.add('loading-none');
+// });
+// or
+const loading = document.getElementById("loading");
+setTimeout(() => {
+  loading.classList.add('loading-none');
+}, 3000);
